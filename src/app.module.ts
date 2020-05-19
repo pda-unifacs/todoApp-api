a628b46567d9d836848a9b1da5f4825e0bcc5100
Reuter Junior <reuter_junior@hotmail.com>
2020-05-19 19:14:01 -0300

    adicionado principais servicos basicos para com a API

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose'
+
+@Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost/todoapp-db')
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
